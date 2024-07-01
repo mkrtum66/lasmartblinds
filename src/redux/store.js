0fc345fad2cb4c04a3a85587c0d4f59a4636a67e
mkrtum <mktum.abrahamyan@icloud.com>
2024-07-01 15:55:18 -0700

    fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,9 @@
-import { applyMiddleware, createStore } from "redux";
-import { rootReducer } from "./rootReducer";
-import { composeWithDevTools } from "@redux-devtools/extension";
-import { thunk } from "redux-thunk";
+import { applyMiddleware, createStore } from 'redux';
 
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk)),
-);
+import { composeWithDevTools } from '@redux-devtools/extension';
+import { thunk } from 'redux-thunk';
+import { rootReducer } from './rootReducer';
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 export default store;
